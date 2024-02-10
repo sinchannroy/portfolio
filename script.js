146eef8b6a21a715fed6940ee8c40cfb6fbaa623
Sinchan <sinchan1509@gmail.com>
2024-02-10 16:12:35 +0530

    Initial Code Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Contact-Button
+$ (".contact-button").click (function () {
+    window.location = "mailto:sinchan1509@gmail.com";
+});
+
+//ContactMe-Button
+$ (".contactme-button").click (function () {
+    window.location = "https://www.linkedin.com/in/sinchannroy/";
+});
+
+//Resume
+$ (".resume-button").click (function () {
+    window.location = "https://drive.google.com/drive/folders/1HSPfM61FyhozpgHC0srzdASRzcjZxKDo?usp=drive_link";
+});
